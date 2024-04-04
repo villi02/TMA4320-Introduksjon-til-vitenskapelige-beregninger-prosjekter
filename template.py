3c239d8b3250dd1775c8a0c4c31940bc08ad11c4
villi02 <villiarnar1@gmail.com>
2024-04-04 11:09:39 +0200

    added template for a method signature

---
+++ template.py
@@ -0,0 +1,18 @@
+## File containing template for how we want the code to be documented and structured
+
+## How functions should be documented
+
+def function_name(arg1, arg2) -> type:
+    """
+    Description of the function
+
+    Parameters:
+    arg1 (type): description of arg1
+    arg2 (type): description of arg2
+
+    Returns:
+    type: description of the return value
+    """
+    # code here
+    pass
+
